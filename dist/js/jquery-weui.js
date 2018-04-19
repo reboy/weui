061db328763f13f61aec2b8f82266509e5fe67a5
Max <Max@Mac-mini.local>
2018-04-19 18:05:24 +0800

    屏幕监听触发

--- dist/js/jquery-weui.js
+++ dist/js/jquery-weui.js
@@ -6556,17 +6556,27 @@ Device/OS Detection
 }($);
 /* global $:true */
 +function ($) {
-  $.fn.fixedtop = function(npx,hei) {
+  $.fn.fixedtop = function(npx,gpx) {
     var $this = $(this);
     var startPos = $this.offset().top;
     $.event.add(window, "scroll", function () {
       var p = $(window).scrollTop();
-      if(npx&&hei){
-        $this.css("position", (p) >= (startPos-npx) ? "fixed" : "static");
-        $this.css("top",(p) >= (startPos-npx)? hei+"px" :"");
+        // $this.css("position", (p) >= (startPos-npx) ? "fixed" : "static");
+      if(npx&&gpx){
+        if((p)>=gpx){
+          $this.css("display","block");
+        }else {
+          $this.css("display","none");
+        }
       }else {
-      $this.css("position", (p) >= startPos ? "fixed" : "static");
-        $this.css('top', ((p) >= startPos) ? '0px' : '');
+        if ((p) >= (startPos - npx)) {
+          $this.css("position", "fixed");
+          $this.prev().css("display", "block");
+        } else {
+          $this.css("position", "static");
+          $this.prev().css("display", "none");
+        }
+        $this.css("top", (p) >= (startPos - npx) ? npx + "px" : "");
       }
     });
   };
