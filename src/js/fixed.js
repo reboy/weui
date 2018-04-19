5b6adccc146481c892858f79aca72130446c6f25
Reboy <1052993999@qq.com>
2018-04-19 18:09:34 +0800

    fix bug

--- src/js/fixed.js
+++ src/js/fixed.js
@@ -1,17 +1,16 @@
 /* global $:true */
 +function ($) {
-  $.fn.fixedtop = function(npx) {
+  $.fn.fixedtop = function (npx, hei) {
     var $this = $(this);
     var startPos = $this.offset().top;
     $.event.add(window, "scroll", function () {
       var p = $(window).scrollTop();
-      if(npx){
-        $this.css("position", (p) > (startPos-npx) ? "fixed" : "static");
-        $this.css("top",npx);
-        $this.css('top', ((p) > startPos-npx) ? npx : '');
-      }else {
-      $this.css("position", (p) > startPos ? "fixed" : "static");
-        $this.css('top', ((p) > startPos) ? '0px' : '');
+      if (npx && hei) {
+        $this.css("position", (p) >= (startPos - npx) ? "fixed" : "static");
+        $this.css("top", (p) >= (startPos - npx) ? hei + "px" : "");
+      } else {
+        $this.css("position", (p) >= startPos ? "fixed" : "static");
+        $this.css('top', ((p) >= startPos) ? '0px' : '');
       }
     });
   };
